90c7a68ddd44bf6190028967c9db838fb4d08d8c
panakj-ag <pankaj@coloredcow.in>
2017-04-04 18:17:21 +0530

    test conflicts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test Conflicts
+                    E - Voting
                 </div>
 
                 <div class="links">
