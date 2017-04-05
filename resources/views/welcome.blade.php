1cb5ea11ee92de766ced13b2dd0c7c1513419cce
metalray7mr <metal.ray7mr@gmail.com>
2017-04-05 22:30:32 +0530

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,11 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Nothing guyub
+                    e-Voting
                         </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">abcdefgh</a>
+                    <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
