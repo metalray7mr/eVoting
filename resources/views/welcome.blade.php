02adb19392623b423955ffb754ebecbe68c87d8d
iamtheuserofthis <iamtheuserofthis@gmail.com>
2017-04-14 18:47:07 +0530

    Changes to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Nothing
+                    Nothing guyub
                         </div>
 
                 <div class="links">
