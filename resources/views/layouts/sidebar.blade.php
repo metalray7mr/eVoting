7a0f0f81e2dfb75ed44518101bd0b21c581b31b8
panakj-ag <pankaj@coloredcow.in>
2017-04-21 15:27:25 +0530

    #initial commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,21 @@
+@if(Auth::check())
+<div class="sidebar_user_info center arial_bold">
+    <h4> {{ Auth::user()->name }} </h4>
+</div>
+<div class="sidebar_menu center">
+    <img src="/images/user.png" class="profile_img_sidebar" onerror="this.src = '{{URL::asset('images/default_profile.png')}}'">
+</div>
+
+<div class="row">
+  <div>
+    <h3> {{ Auth::user()->name }}</h3>
+  </div>
+
+  <div>
+    <h3> {{ Auth::user()->email }}</h3>
+  </div>
+
+</div>
+
+
+@endif
